82dbffb0c115404ab696ac08eefbb8ffd58fa5c5
muhammad230 <ali.khansmr3@gmail.com>
2025-10-23 21:18:29 +0500

    add image and some html

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./component/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Hero/>  
+
+    </div>
+  )
+};
+
+export default App;
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,5 +1,18 @@
 const Hero = () =>{
-    return
+    return (
+        <nav>
+            <div className="logo">
+                <img src="../images/brand_logo (1).png" alt="logo" />
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+            <button>login</button>
+        </nav>
+    )
 }
 
 export default Hero;
