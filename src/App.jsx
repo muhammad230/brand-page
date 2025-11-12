656602022b4b25a4ecd8219e07323ca28745a1ce
muhammad230 <ali.khansmr3@gmail.com>
2025-11-12 17:44:59 +0500

    add a toggle mode or popup method

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import "./App.css";
 import Hero from "./component/Hero";
 import Navigation from "./component/Navigation";
-
-
+import { useState } from "react";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const toggleLogin = () => {
+    setIsLogin((prev) => !prev);
+  };
+
   return (
     <div>
-    <Navigation/>
-    <Hero/>
+      <Navigation toggle={toggleLogin} />
+      <Hero />
 
+      
+      {isLogin && (
+        <div className="popup">
+          <div className="popup-content">
+            <h2>Login</h2>
+            <p>Please enter your login details.</p>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <div className="popup-buttons">
+              <button>Submit</button>
+              <button className="btn-2" onClick={toggleLogin}>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 };
 
 export default App;
--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -1,18 +1,18 @@
-const Navigation = () =>{
-    return (
-        <nav>
-            <div className="logo">
-                <img src="../images/brand_logo (1).png" alt="logo" />
-            </div>
-            <ul>
-                <li href="#">Menu</li>
-                <li href="#">Location</li>
-                <li href="#">About</li>
-                <li href="#">Contact</li>
-            </ul>
-            <button>login</button>
-        </nav>
-    )
-}
+const Navigation = ({ toggle }) => {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="../images/brand_logo (1).png" alt="logo" />
+      </div>
+      <ul>
+        <li>Menu</li>
+        <li>Location</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <button onClick={toggle}>Login</button>
+    </nav>
+  );
+};
 
 export default Navigation;
